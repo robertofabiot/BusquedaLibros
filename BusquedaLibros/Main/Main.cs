d1faff8fc5b1b901a547838454351dea6dcb7871
robertofabiot <rftercerom@uamv.edu.ni>
2025-11-24 00:34:18 -0600

    Add ImprimirMenu method

--- BusquedaLibros/Main/Main.cs
+++ BusquedaLibros/Main/Main.cs
@@ -1,10 +1,19 @@
-﻿namespace BusquedaLibros.Main
+﻿using BusquedaLibros.DAO;
+
+namespace BusquedaLibros.Main
 {
     internal class Program
     {
+        static void ImprimirMenu()
+        {
+            Console.WriteLine("1. Búsqueda lineal en la lista de libros.");
+            Console.WriteLine("2. Búsqueda binaria en la lista de autores.");
+            Console.WriteLine("3. Buscar libro más reciente y más antiguo.");
+            Console.WriteLine("4. Buscar coincidencia dentro de descripciones textuales.");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
         }
     }
 }
