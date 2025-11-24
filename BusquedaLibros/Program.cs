0afa2695df2c3e216a250d2d768eb3731c91c96d
robertofabiot <rftercerom@uamv.edu.ni>
2025-11-23 23:30:56 -0600

    Add project files.

---
+++ BusquedaLibros/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BusquedaLibros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
