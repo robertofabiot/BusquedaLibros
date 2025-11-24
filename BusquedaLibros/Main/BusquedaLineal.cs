788f4b09390c755d3398199ef9da24dec730cb1e
robertofabiot <rftercerom@uamv.edu.ni>
2025-11-24 00:38:50 -0600

    Add BusquedaLineal y BusquedaBinaria files for future branching

---
+++ BusquedaLibros/Main/BusquedaLineal.cs
@@ -0,0 +1,17 @@
+﻿using BusquedaLibros.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusquedaLibros.Main
+{
+    public class BusquedaLineal
+    {
+        public static int BuscarPorNombre(List<Libro> libros, string nombre)
+        {
+            return 0;
+        }
+    }
+}
